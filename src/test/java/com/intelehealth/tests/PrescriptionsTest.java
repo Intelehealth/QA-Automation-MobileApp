81228737282a63e9b5ad4e36abe91d226fff1149
SrinivasBandi9 <147377159+SrinivasBandi9@users.noreply.github.com>
2025-05-16 14:00:17 +0530

    Updated some code

--- src/test/java/com/intelehealth/tests/PrescriptionsTest.java
+++ src/test/java/com/intelehealth/tests/PrescriptionsTest.java
@@ -1,18 +1,17 @@
 package com.intelehealth.tests;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.lang.reflect.Method;
 import java.util.Locale;
 
 import org.json.JSONObject;
 import org.json.JSONTokener;
-import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import com.github.javafaker.Faker;
-import com.intelehealth.api.APIServices;
 import com.intelehealth.base.BaseTest;
 import com.intelehealth.pages.AddNewPatientPage;
 import com.intelehealth.pages.AppSetupPage;
@@ -24,8 +23,10 @@ import com.intelehealth.pages.StartVisit1And2StepsPage;
 import com.intelehealth.pages.VisitSummaryPage;
 import com.intelehealth.utils.TestUtils;
 
-public class PrescriptionsTest extends BaseTest {
 
+public class PrescriptionsTest extends BaseTest{
+	
+	TestUtils utils = new TestUtils();
 	AppSetupPage appSetupPage;
 	// JSON object to store test data
 	JSONObject appData;
@@ -42,19 +43,13 @@ public class PrescriptionsTest extends BaseTest {
 
 	@BeforeMethod
 	public void beforeMethod(Method m) throws Throwable {
-		TestUtils.log().info("\n" + "****** starting test:" + m.getName() + "******" + "\n");
+		utils.log().info("\n" + "****** starting test:" + m.getName() + "******" + "\n");
 		resetApp();
 		launchApp();
 		appSetupPage = new AppSetupPage();
 		findPatientPage = new FindPatientPage();
 		prescriptionsPage = new PrescriptionsPage();
 		InputStream datais = null;
-		/**
-		 * GOing forward we will be using these variables for authorization for CHW and
-		 * Doctor
-		 */
-		hw_request = buildRequestWithNurseAuthorization();
-		doctor_request = buildRequestWithDoctorAuthorization();
 		try {
 			String dataFileName = "data/appData.json";
 			datais = getClass().getClassLoader().getResourceAsStream(dataFileName);
@@ -68,125 +63,113 @@ public class PrescriptionsTest extends BaseTest {
 				datais.close();
 			}
 		}
-		// grant all permissions
+		//grant all permissions
 		appSetupPage.handlePermissions();
 		// Perform the complete setup using the obtained username and password
-		appSetupPage.completeSetup();
+        appSetupPage.completeSetup();
 
-	}
-
+}
+	
 	@Test(priority = 1, description = "Verify received prescriptions patients listed in recent visit", enabled = true)
 	public void IDA4_2078_verifyReceivedPrescriptionsPatientsListedInRecentVisit() throws Throwable {
-		/**
-		 * GOing forward we will be using these methods for visit creation as CHW,
-		 * starting the visit as doctor, sign and submit
-		 */
-		APIServices.createVisitUsingRestAssured(hw_request);
-		APIServices.startVisitUsingRestAssured(doctor_request);
-		APIServices.signAndSubmitUsingRestAssured(doctor_request);
-
-		Assert.assertTrue(prescriptionsPage.verifyReceivedPrescriptionsPatientsListedInRecentVisit());
+		prescriptionsPage.verifyReceivedPrescriptionsPatientsListedInRecentVisit();
 	}
-
+	
 	@Test(priority = 15, description = "Verify share functionality when clicked on any recent patient visit", enabled = true)
 	public void IDA4_2079_verifyShareFunctionalityWhenClickedOnAnyRecentPatientVisit() throws Throwable {
-		Assert.assertTrue(prescriptionsPage.verifyShareFunctionalityWhenClickedOnAnyRecentPatientVisit());
+		prescriptionsPage.verifyShareFunctionalityWhenClickedOnAnyRecentPatientVisit();
 	}
-
+	
 	@Test(priority = 3, description = "Verify if the prescription is shared on entering a valid number", enabled = true)
 	public void IDA4_2080_verifyIfThePrescriptionIsSharedOnEnteringAValidNumber() throws Throwable {
-		Assert.assertTrue(prescriptionsPage.verifyIfThePrescriptionIsSharedOnEnteringAValidNumber());
+		prescriptionsPage.verifyIfThePrescriptionIsSharedOnEnteringAValidNumber();
 	}
-
+	
 	@Test(priority = 4, description = "Verify the count of patients awaiting prescriptions are correct", enabled = true)
 	public void IDA4_2085_verifyTheCountOfPatientsAwaitingPrescriptionsAreCorrect() throws Throwable {
-		Assert.assertTrue(prescriptionsPage.verifyTheCountOfPatientsAwaitingPrescriptionsAreCorrect());
+		prescriptionsPage.verifyTheCountOfPatientsAwaitingPrescriptionsAreCorrect();
 	}
-
+	
 	@Test(priority = 5, description = "Verify that user is able to navigate to visit details page by clicking on any recent visit", enabled = true)
-	public void IDA4_2086_verifyThatUserIsAbleToNavigateToVisitDetailsPageByClickingOnAnyRecentVisit()
-			throws Throwable {
-		Assert.assertTrue(
-				prescriptionsPage.verifyThatUserIsAbleToNavigateToVisitDetailsPageByClickingOnAnyRecentVisit());
+	public void IDA4_2086_verifyThatUserIsAbleToNavigateToVisitDetailsPageByClickingOnAnyRecentVisit() throws Throwable {
+		prescriptionsPage.verifyThatUserIsAbleToNavigateToVisitDetailsPageByClickingOnAnyRecentVisit();
 	}
-
+	
 	@Test(priority = 6, description = "Verify that user can call patient", enabled = true)
 	public void IDA4_2089_verifyThatUserCanCallPatient() throws Throwable {
-		Assert.assertTrue(prescriptionsPage.verifyThatUserCanCallPatient());
+		prescriptionsPage.verifyThatUserCanCallPatient();
 	}
-
+	
 	@Test(priority = 7, description = "Verify that user can send whatsapp message to the patient", enabled = true)
 	public void IDA4_2090_verifyThatUserCanSendWhatsappMessageToThePatient() throws Throwable {
-		Assert.assertTrue(prescriptionsPage.verifyThatUserCanSendWhatsappMessageToThePatient());
+		prescriptionsPage.verifyThatUserCanSendWhatsappMessageToThePatient();
 	}
-
+	
 	@Test(priority = 8, description = "Verify user is able to view visit summary page", enabled = true)
 	public void IDA4_2092_verifyUserIsAbleToViewVisitSummaryPage() throws Throwable {
-		Assert.assertTrue(prescriptionsPage.verifyUserIsAbleToViewVisitSummaryPage());
+		prescriptionsPage.verifyUserIsAbleToViewVisitSummaryPage();
 	}
-
+	
 	@Test(priority = 9, description = "Verify that user can navigate to prescription page", enabled = true)
 	public void IDA4_2094_verifythatUserCanNavigateToPrescriptionPage() throws Throwable {
-		Assert.assertTrue(prescriptionsPage.verifythatUserCanNavigateToPrescriptionPage());
+		prescriptionsPage.verifythatUserCanNavigateToPrescriptionPage();
 	}
-
+	
 	@Test(priority = 10, description = "Verify that user is able to download the prescription", enabled = true)
 	public void IDA4_2099_verifyThatUserIsAbleToDownloadThePrescription() throws Throwable {
-		Assert.assertTrue(prescriptionsPage.verifyThatUserIsAbleToDownloadThePrescription());
+		prescriptionsPage.verifyThatUserIsAbleToDownloadThePrescription();
 	}
-
+	
 	@Test(priority = 11, description = "Verify user can print the prescription", enabled = true)
 	public void IDA4_2100_verifyUserCanPrintThePrescription() throws Throwable {
-		Assert.assertTrue(prescriptionsPage.verifyUserCanPrintThePrescription());
+		prescriptionsPage.verifyUserCanPrintThePrescription();
 	}
-
+	
 	@Test(priority = 12, description = "Verify that user can share the prescription to patient", enabled = true)
 	public void IDA4_2101_verifyThatUserCanShareThePrescriptionToPatient() throws Throwable {
-		// change method name to, mobile number pre filled
 		prescriptionsPage.visitDetailsPrescriptionShare();
-		Assert.assertTrue(prescriptionsPage.verifyThatUserCanShareThePrescriptionToPatient());
+		prescriptionsPage.verifyThatUserCanShareThePrescriptionToPatient();
 	}
-
+	
 	@Test(priority = 13, description = "Verify user can share through whatsapp", enabled = true)
 	public void IDA4_2102_verifyUserCanShareThroughWhatsapp() throws Throwable {
-
 		prescriptionsPage.visitDetailsPrescriptionShare();
-		Assert.assertTrue(prescriptionsPage.verifyUserCanShareThroughWhatsapp());
+		prescriptionsPage.verifyUserCanShareThroughWhatsapp();
 	}
-
+	
 	@Test(priority = 14, description = "Verify Home navigates to home page", enabled = true)
 	public void IDA4_2104_verifyHomeNavigatesToHomePage() throws Throwable {
-		Assert.assertTrue(prescriptionsPage.verifyHomeNavigatesToHomePage());
+		prescriptionsPage.verifyHomeNavigatesToHomePage();
 	}
-
+	
 	@Test(priority = 18, description = "Verify that user can End visit", enabled = true)
 	public void IDA4_2105_verifyThatUserCanEndVisit() throws Throwable {
-		Assert.assertTrue(prescriptionsPage.verifyThatUserCanEndVisitFromPrescritionScreen());
+		prescriptionsPage.verifyThatUserCanEndVisit();
 	}
-
+	
 	@Test(priority = 19, description = "Verify user can end visit", enabled = true)
 	public void IDA4_2106_verifyUserCanEndVisit() throws Throwable {
-		Assert.assertTrue(prescriptionsPage.verifyUserCanEndVisitFromVisitDetailsScreen());
+		prescriptionsPage.verifyUserCanEndVisit();
 	}
-
+	
 	@Test(priority = 2, description = "Verify user will get the follow up alert pop up when End the visit", enabled = true)
 	public void IDA4_2107_verifyUserWillGetTheFollowUpAlertPopUpWhenEndTheVisit() throws Throwable {
-		Assert.assertTrue(prescriptionsPage.verifyUserWillGetTheFollowUpAlertPopUpWhenEndTheVisit());
+		prescriptionsPage.verifyUserWillGetTheFollowUpAlertPopUpWhenEndTheVisit();
 	}
-
+	
 	@Test(priority = 16, description = "Verify the Awaiting prescription notification is displayed along with the count", enabled = true)
 	public void IDA4_2109_verifyTheAwaitingPrescriptionNotificationIsDisplayedAlongWithTheCount() throws Throwable {
-		Assert.assertTrue(prescriptionsPage.verifyTheAwaitingPrescriptionNotificationIsDisplayedAlongWithTheCount());
+		prescriptionsPage.verifyTheAwaitingPrescriptionNotificationIsDisplayedAlongWithTheCount();
 	}
-
+	
 	@Test(priority = 17, description = "Verify that recent visits display in list", enabled = true)
 	public void IDA4_2110_verifyThatRecentVisitsDisplayInList() throws Throwable {
-		Assert.assertTrue(prescriptionsPage.verifyThatRecentVisitsDisplayInList());
+		prescriptionsPage.verifyThatRecentVisitsDisplayInList();
 	}
-
+	
 	@Test(priority = 20, description = "Verify that user can end visit", enabled = true)
 	public void IDA4_2113_verifyThatUserCanEndPendingVisit() throws Throwable {
-		Assert.assertTrue(prescriptionsPage.verifyThatUserCanEndPendingVisit());
+		prescriptionsPage.verifyThatUserCanEndPendingVisit();
 	}
 
 	@AfterMethod
